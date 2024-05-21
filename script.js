2d2c507cd21178379f8167042ec167d73890aaa3
Ali Hamza <hamzamashooq492@gmail.com>
2024-05-21 13:03:50 +0500

    Add Toaster

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let clearInput = value => document.getElementById(value).value = ""
 function handleRegisterCard() {
     document.getElementById('login-card').style.display = "none";
     document.getElementById('register-card').style.display = "inline-block"
-    
-    
+
+
 }
 
 
@@ -29,20 +29,23 @@ function handleLoginBtn() {
     let password = getValue('password')
     email = email.trim()
     if (email === "" || password === "") {
-        alert("Please Enter email and password")
+        // alert("Please Enter email and password")
+        toast("Please enter Email and Password", "error")
     } else {
         if (users.find(user => user.email === email && user.password === password)) {
-            
-            alert("login Successfully")
+
+            // alert("Login Successfully")
+            toast("login Successfully", "success")
             clearInput("email")
             clearInput("password")
         } else {
-            alert("Invalid email or password")
+            // alert("Invalid email or password")
+            toast("Invalid email or password", "warn")
         }
     }
     // console.log(email,password);
-  
-    
+
+
 }
 
 
@@ -51,20 +54,64 @@ function handleRegisterBtn() {
     let password = getValue('register-password')
     email = email.trim()
     if (email === "" || password === "") {
-        alert("Please Enter email and password")
+        // alert("Please Enter email and password")
+        toast("Please enter Email and Password", "error")
     } else {
         if (users.find(user => user.email === email)) {
 
-            alert("Already have account")
+            // alert("Already have account")
+            toast("Already have account", "warn")
         } else {
             let user = { email, password }
 
             console.log(email, password);
             users.push(user);
             console.log(users);
-            alert("registered Successfully")
+            // alert("registered Successfully")
+            toast("Registered Successfully", "success")
         }
     }
     clearInput("register-email")
     clearInput("register-password")
+}
+
+
+
+// toastify function
+function toast(note, key) {
+    let color1;
+    let color2;
+    switch (key) {
+        case "error":
+            color1 = "#C71585"
+            color2 = "#09203F"
+            break;
+        case "warn":
+            color1 = "#d02d2d"
+            color2 = "#09203F"
+            break;
+        case "success":
+            color1 = "#008000"
+            color2 = "#09203F"
+            break;
+        default:
+            color1 = "#FFFFFF"
+            color2 = "#F0F0F0"
+    }
+
+    Toastify({
+        text: note,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: `linear-gradient(to right, ${color1},${color2}`,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+
 }
